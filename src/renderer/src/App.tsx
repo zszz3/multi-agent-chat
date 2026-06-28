42432722109486ad8b53c7bcfbff78b95545a5a4
zhaipengjie <355251@whut.edu.cn>
2026-06-29 01:12:49 +0800

    refactor: narrow app compatibility helper exports

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,22 @@
 import { AppShell } from "./AppShell";
 
 export { AppShell as App } from "./AppShell";
+export {
+  applyProviderModelIdToAgentConfig,
+  applyProviderPresetToConfiguredAgent,
+  shouldRefreshBalances,
+  workflowArtifactSummary,
+  workflowContextDocumentFromArtifacts,
+  workflowDraftShouldPersist,
+  workflowTaskLiveDetail,
+} from "./app/app-state";
+export {
+  parseWorkflowJudgeResult,
+  workflowFinalReviewPrompt,
+  workflowJudgePrompt,
+  workflowNodeRunPrompt,
+  workflowProgressAfterFailure,
+} from "./pages/workflow/workflow-domain";
 export * from "./AppShell";
 
 export default AppShell;
